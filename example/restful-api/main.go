0cf679f929fafcfcec6a14f3eba4902b07de2066
Mong MX <mongmx@gmail.com>
2019-02-13 15:40:20 +0700

    refactor code

--- example/restful-api/main.go
+++ example/restful-api/main.go
@@ -8,68 +8,65 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
-type (
-	user struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	}
-)
+type book struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 var (
-	users = map[int]*user{}
+	books map[int]*book
 	seq   = 1
 )
 
-//----------
-// Handlers
-//----------
-
-func createUser(c echo.Context) error {
-	u := &user{
-		ID: seq,
-	}
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	users[u.ID] = u
-	seq++
-	return c.JSON(http.StatusCreated, u)
-}
-
-func getUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	return c.JSON(http.StatusOK, users[id])
-}
-
-func updateUser(c echo.Context) error {
-	u := new(user)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	id, _ := strconv.Atoi(c.Param("id"))
-	users[id].Name = u.Name
-	return c.JSON(http.StatusOK, users[id])
-}
-
-func deleteUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	delete(users, id)
-	return c.NoContent(http.StatusNoContent)
-}
-
 func main() {
+	books = make(map[int]*book)
 	e := echo.New()
 
 	// Middleware
 	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
 
 	// Routes
-	e.POST("/users", createUser)
-	e.GET("/users/:id", getUser)
-	e.PUT("/users/:id", updateUser)
-	e.DELETE("/users/:id", deleteUser)
+	e.POST("/book", createBook)
+	e.GET("/books", listBook)
+	e.GET("/book/:id", getBook)
+	e.PUT("/book/:id", updateBook)
+	e.DELETE("/book/:id", deleteBook)
 
 	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Start(":8080")
+}
+
+//----------
+// Handlers
+//----------
+func createBook(c echo.Context) error {
+	b := &book{}
+	c.Bind(b)
+	b.ID = seq
+	books[b.ID] = b
+	seq++
+	return c.JSON(http.StatusCreated, b)
+}
+
+func listBook(c echo.Context) error {
+	return c.JSON(http.StatusOK, books)
+}
+
+func getBook(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, books[id])
+}
+
+func updateBook(c echo.Context) error {
+	b := new(book)
+	c.Bind(b)
+	id, _ := strconv.Atoi(c.Param("id"))
+	books[id].Name = b.Name
+	return c.JSON(http.StatusOK, books[id])
+}
+
+func deleteBook(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(books, id)
+	return c.NoContent(http.StatusNoContent)
 }
